13479b331fb72e9f57876f99b5234ad63bbb025a
Cervantes <yoselyc@88665a13d974.ant.amazon.com>
2021-06-18 09:37:01 -0700

    test

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Z-finity</h1>
+        <p>Help Services</p>
         
         {/* <a
           className="App-link"
